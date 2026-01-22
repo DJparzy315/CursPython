838b519cdaab34cf3518aad7e62a8c73b811ac5a
Ovidiu Gligorescu <ovidiu315@yahoo.com>
2026-01-22 09:11:43 +0200

    fix

--- L1/ConversieCelsiusToFarenheit.py
+++ L1/ConversieCelsiusToFarenheit.py
@@ -1,3 +1,15 @@
-c = float(input("Celsius: "))
-f = c * 9/5 + 32
-print("Farenheit:", f)
+def este_float(text):
+    try:
+        float(text)
+        return True
+    except:
+        return False
+
+while True:
+    c = input("Celsius: ")  # citim ca string
+    if este_float(c):
+        f = float(c) * 9/5 + 32
+        print("Fahrenheit:", f)
+        break  # daca vrei sa se opreasca dupa o valoare valida
+    else:
+        print("Te rog introdu un numar valid.")
